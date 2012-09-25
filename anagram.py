165d1a44992ebd68e79c8b80b10791f44ad4d247
Robby Colvin <geetarista@gmail.com>
2012-09-24 21:27:45 -0700

    Initial commit

---
+++ anagram.py
@@ -0,0 +1,35 @@
+#! /usr/env/bin python
+
+import sys
+
+if len(sys.argv) < 2:
+    print "Please pass in a file. Examples:"
+    print ""
+    print "  /usr/share/dict/words"
+    print "  words.txt"
+    sys.exit()
+
+word_groups = {}
+
+filename = sys.argv[1]
+
+try:
+    file = open(filename)
+except IOError:
+    print "%s is not a valid file." % filename
+    sys.exit()
+
+text = file.readlines()
+ 
+for word in text:
+    word = word.rstrip("\n")
+    group = "".join(sorted(list(word)))
+    if group in word_groups:
+        word_groups[group].append(word)
+    else:
+        word_groups[group] = [word]
+
+for group in word_groups.values():
+    print ", ".join(group)
+
+file.close()
---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/process', methods=['GET', 'POST'])
+def process():
+    file = request.files['file']
+    
+    text = file.readlines()
+
+    word_groups = {}
+     
+    for word in text:
+        word = word.rstrip("\n")
+        group = "".join(sorted(list(word)))
+        if group in word_groups:
+            word_groups[group].append(word)
+        else:
+            word_groups[group] = [word]
+
+    return render_template("process.html", word_groups=word_groups)
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
